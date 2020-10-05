40a053f92b11b806001b14116c58696ba7f4a8b2
Joar <joar.alstad@learnet.se>
2020-10-05 09:38:46 +0200

    first commit

---
+++ Prov 1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Prov_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+
+            Console.WriteLine("Välkommen till Sänka Skepp");
+
+            while (i > 0)
+            {
+                Console.WriteLine("Välj en kordinat mellan [1-10]");
+
+                {
+                    string a = Console.ReadLine();
+                    if (a == "7")
+                    {
+                        Console.WriteLine("Hit!");
+                        i -= 1;
+                    }
+                    else if (a == "1" | a == "2" | a == "3" | a == "4" | a == "5" | a == "6" | a == "8" | a == "9" | a == "10")
+                    {
+                        Console.WriteLine("Miss!");
+
+                    }
+                    else if (a != "1" | a != "2" | a != "3" | a != "4" | a != "5" | a != "6" | a != "7" | a != "8" | a != "9" | a != "10")
+                    {
+                        Console.WriteLine("Inte ett tal, försök igen");
+                    }
+
+
+                    Console.ReadLine();
+
+
+
+
+                }
+            }
+
+        }
+    }
+}
+
+
+
+
+
+
+
